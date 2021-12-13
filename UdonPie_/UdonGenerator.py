324808d3f960377ed799b2d61f7b10df02d4cca0
SmewVR <93958928+SmewVR@users.noreply.github.com>
2021-12-13 00:10:31 -0600

    Update UdonGenerator.py

--- UdonPie_/UdonGenerator.py
+++ UdonPie_/UdonGenerator.py
@@ -1,16 +1,53 @@
 
+import ast
 
+source_code = """
+import udon_types
 
+a = udon_types.udon_types["Material"]
+b = 'sdf'
+
+def sum():
+    return a + b
+"""
 
 
 variables = []
+functions = []
 
+tree = ast.parse(source_code)
+
+for node in ast.walk(tree):
+    # VARIABLE NAME
+    if isinstance(node, ast.Name):
+        variables.append(node.id)
+        print(node.id)
+
+    # VARIABLE VALUES
+    if isinstance(node, ast.Constant):
+        print(node.value)
+    # FUNCTION NAMES
+    if isinstance(node, ast.FunctionDef):
+        functions.append(node.name)
+        print(node.name)
+
+print(variables)
+print(functions)
 
 def make_code_seg(self) -> str:
-    ret_code: str = f'.code_start\n\n'
+    ret_code: str = f'.data_start\n\n'
 
     for variable in variables:
         ret_code += '.export '+variable+'\n'
+
+    ret_code += '.data_end ' + variable + '\n'
+    ret_code += '.code_start ' + variable + '\n'
+    for function in functions:
+        ret_code += '.export ' + variable + '\n'
+
+
+
     ret_code += '\n'
+
     ret_code += f'.code_end\n'
     return ret_code
